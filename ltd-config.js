7c2ae2e4944f74b5f28da9c7bf4a336c3a783377
ltdrobot <localizethedocs@gmail.com>
2026-09-22 13:04:15 +0000

    Deploying to pages from @ ltdorgtest/mdbook-docs-l10n@5e74c1915f5151754c1df209ebce2fa4b172e890 🚀

---
+++ ltd-config.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["master",  "Development"],
+    ["0.5",     "Release 0.5"],
+    ["0.4",     "Release 0.4"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.ltdorgtest.org"],
+    ["Crowdin", "https://ltdorgtest.crowdin.com/mdbook-docs-l10n"],
+    ["GitHub",  "https://github.com/ltdorgtest/mdbook-docs-l10n"],
+    ["GitCode", "https://gitcode.com/ltdorgtest/mdbook-docs-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/ltdorgtest/mdbook-docs-l10n"],
+  ]
+};
